b6fa63481874938a9de21cb18a902e7d1a696fa2
Haris Wasim <toughrider617@gmail.com>
2025-11-15 19:44:41 +0500

    Refactor binary tree structure and methods
    Refactor binary tree implementation to use a single tree struct. Update insert, inOrder, preOrder, and postOrder methods accordingly.

--- Trees/Binary-Tree-Implementation.cpp
+++ Trees/Binary-Tree-Implementation.cpp
@@ -1,71 +1,107 @@
 #include <iostream>
 using namespace std;
 
-struct node
-{
-	int data;
-	node* left;
-	node* right;
-
-	node()
-	{
-		data = 0;
-		left = right = nullptr;
-	}
-};
-
 struct tree
 {
-	node* root;
+    int data;
+    tree* left;
+    tree* right;
 
-	tree()
-	{
-		root = nullptr;
-	}
+    tree()
+    {
+        data = 0;
+        left = right = nullptr;
+    }
 
-	node* insert(node *root, int val)
-	{
-		if (!root)
-		{
-			node* n = new node;
-			n->data = val;
-			root = n;
-		}
+    tree* insert(tree* root, int data)
+    {
+        if (!root)
+        {
+            tree* t = new tree;
+            t->data = data;
+            root = t;
+        }
 
-		else if (val >= root->data)
-		{
-			root->right = insert(root->right, val);
-		}
+        else if (data > root->data)
+        {
+            root->right = insert(root->right, data);
+        }
 
-		else if (val < root->data)
-		{
-			root->left = insert(root->left, val);
-		}
+        else
+        {
+            root->left = insert(root->left, data);
+        }
 
-		return root;
-	}
+        return root;
+    }
 
-	void insert(int val)
-	{
-		root = insert(root, val);
-	}
+    void inOrder(tree* root)
+    {
+        if (!root)
+        {
+            return;
+        }
+
+        inOrder(root->left);
+        cout << root->data << endl;
+        inOrder(root->right);
+
+    }
+
+    void preOrder(tree* root)
+    {
+        if (!root)
+        {
+            return;
+        }
+
+        cout << root->data << endl;
+        preOrder(root->left);
+        preOrder(root->right);
+
+    }
+
+    void postOrder(tree* root)
+    {
+        if (!root)
+        {
+            return;
+        }
+
+        postOrder(root->left);
+        postOrder(root->right);
+        cout << root->data << endl;
+
+    }
 
 };
-
-
 
 int main()
 {
-	tree t;
+    tree t1;
+    tree* t1root = nullptr;
 
-	t.insert(20);
-	t.insert(10);
-	t.insert(30);
+    t1root = t1.insert(t1root, 10);
 
-	cout << t.root->data << endl;
-	cout << t.root->left->data << endl;
-	cout << t.root->right->data << endl;
+    t1.insert(t1root, 5);
+    t1.insert(t1root, 6);
+    t1.insert(t1root, 2);
+    t1.insert(t1root, 15);
+    t1.insert(t1root, 20);
+    t1.insert(t1root, 25);
 
+    t1.insert(t1root, 14);
+    t1.insert(t1root, 13);
+    t1.insert(t1root, 18);
 
-	return 0;
+    cout << "InOrder:" << endl;
+    t1.inOrder(t1root);
+
+    cout << "\nPreOrder:" << endl;
+    t1.preOrder(t1root);
+
+    cout << "\nPostOrder:" << endl;
+    t1.postOrder(t1root);
+
+    return 0;
 }
